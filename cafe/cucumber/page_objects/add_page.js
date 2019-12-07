6baab622460cfd386025eb1c253041d4c618bb25
Unknown <suthanhari2006@gmail.com>
2019-12-07 08:23:39 +0100

    add employee feature

--- cafe/cucumber/page_objects/add_page.js
+++ cafe/cucumber/page_objects/add_page.js
@@ -1,10 +1,46 @@
 'use strict'
-var CalculatorPage = function() {
+var CafePage = function() {
 
-  this.get = function() {
-    browser.get('http://cafetownsend-angular-rails.herokuapp.com/login');
-  };
+    this.get = function () {
+        browser.get('http://cafetownsend-angular-rails.herokuapp.com/login');
+    };
 
+    this.enterUser = function (value) {
+        element(by.model('user.name')).sendKeys(value);
+    };
+
+    this.enterPass = function (value) {
+        element(by.model('user.password')).sendKeys(value);
+    };
+
+    this.clickGo = function () {
+        element(by.buttonText('Login')).click()
+
+    }
+
+    this.create = function () {
+        element(by.id('bAdd')).click()
+    };
+    this.enterFirst = function (value) {
+        element(by.model('selectedEmployee.firstName')).sendKeys(value);
+    };
+
+    this.enterLast = function (value) {
+        element(by.model('selectedEmployee.lastName')).sendKeys(value);
+    };
+
+    this.enterDate = function (value) {
+        element(by.model('selectedEmployee.startDate')).sendKeys(value);
+    };
+
+    this.enterEmail = function (value) {
+        element(by.model('selectedEmployee.email')).sendKeys(value);
+
+    };
+
+    this.clickAdd = function () {
+        element(by.buttonText('Add')).click()
+
+    };
 };
-
-module.exports = CalculatorPage;
+module.exports = CafePage;
--- cafe/cucumber/step_definitions/add_steps.js
+++ cafe/cucumber/step_definitions/add_steps.js
@@ -2,19 +2,40 @@ var chai = require('chai').use(require('chai-as-promised'));
 var expect = chai.expect;
 browser.driver.manage().window().maximize();
 
-var CalculatorSteps = function() {
+var CafeSteps = function() {
 
-  var CalculatorPage = require("../page_objects/add_page.js");
+  var CafePage = require("../page_objects/add_page.js");
 
   this.World = function MyWorld() {
-    this.page = new CalculatorPage();
+    this.page = new CafePage();
   };
 
-  this.Given('The cafeTown page is open', function (callback) {
+  this.Given(/^The cafeTown page is open$/, function (callback) {
     this.page.get();
     callback();
   });
 
+    this.When(/^I enter the login details as (.*) and (.*) and click$/, function (username, password, callback) {
+    this.page.enterUser(username);
+    this.page.enterPass(password);
+    this.page.clickGo();
+    callback();
+   });
+
+    this.Given(/^I click create to add the employee$/, function (callback) {
+        this.page.create();
+        callback();
+    });
+
+
+    this.When(/^I enter the details of the employe as (.*) and (.*) and (.*) and (.*)$/, function (firstname, lastname, startdate, email, callback) {
+        this.page.enterFirst(firstname);
+        this.page.enterLast(lastname);
+        this.page.enterDate(startdate);
+        this.page.enterEmail(email);
+        this.page.clickAdd();
+        callback();
+    });
 };
 
-module.exports = CalculatorSteps;
+module.exports = CafeSteps;
